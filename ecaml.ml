4d789fd79cdbd9fc8df29c62eb3065905018f451
kkeundotnet <kkeundotnet@gmail.com>
2020-04-02 01:57:42 +0100

    Merge ecaml.ml and parsercomb.ml

--- ecaml.ml
+++ ecaml.ml
@@ -17,78 +17,108 @@
 
 open ExtLib
 open Printf
-open Parsercomb
 
-type template_content =
+type elem =
   | Text of string
   | BlockOfCode of string
   | BlockOfExpr of string
   | BlockOfRawExpr of string
   | LineOfCode of string
 
-type template = template_content list
+type t = elem list
 
-let whitespace = function ' ' | '\t' -> true | _ -> false
+let print ~printer ~esc_printer ch =
+  List.iter (function
+    | LineOfCode s -> fprintf ch "%s\n" s
+    | BlockOfCode s -> fprintf ch "%s" s
+    | BlockOfExpr s -> fprintf ch "%s (%s);\n" esc_printer s
+    | BlockOfRawExpr s -> fprintf ch "%s (%s);\n" printer s
+    | Text s -> fprintf ch "%s %S;\n" printer s)
 
-let p_whitespace = p_pred whitespace
+(* Continuation *)
+
+type 'a parsed = Parsed of { v : 'a; st : string * int } | Failed
+
+let parsed v st = Parsed { v; st }
+
+let ( ||| ) p1 p2 x =
+  match p1 x with Parsed _ as parsed -> parsed | Failed -> p2 x
+
+let ( >>| ) p f x =
+  match p x with Parsed { v; st } -> Parsed { v = f v; st } | Failed -> Failed
+
+let ( >>> ) p1 p2 x =
+  match p1 x with Parsed { v = (); st } -> p2 st | Failed -> Failed
+
+(* Parse *)
+
+let p_pred p (s, n) =
+  assert (n >= 0);
+  if n < String.length s && p s.[n] then Parsed { v = (); st = (s, succ n) }
+  else Failed
+
+(** parsed if current position = p; doesn't jump *)
+let p_pos x ((s, pos) as st) = if x = pos then Parsed { v = (); st } else Failed
+
+let p_whitespace = p_pred (function ' ' | '\t' -> true | _ -> false)
+
+let p_char c = p_pred (( = ) c)
+
+(* checks previous char; doesn't jump *)
+let p_prev_char c (s, pos) = if pos <= 0 then Failed else p_char c (s, pred pos)
+
+let p_str str = String.fold_left (fun p c -> p >>> p_char c) (parsed ()) str
+
+let p_until_parsed p ((s, pos) as st) =
+  let len = String.length s in
+  let first = pos in
+  let rec loop ((s, pos) as st) =
+    if pos >= len then Failed
+    else
+      match p st with
+      | Parsed { v; st = (s, _) as st } ->
+          Parsed { v = (String.slice ~first ~last:pos s, v); st }
+      | Failed -> loop (s, succ pos)
+  in
+  loop st
+
+let p_until_char_parsed c = p_until_parsed (p_char c) >>| fun (v, _c) -> v
+
+let p_until_str_parsed str = p_until_parsed (p_str str) >>| fun (v, _str) -> v
+
+let p_until_failed p st =
+  let rec loop st =
+    match p st with
+    | Parsed { st; _ } -> loop st
+    | Failed -> Parsed { v = (); st }
+  in
+  loop st
+
+let code_fragment =
+  p_str "<%="
+  >>> ( p_str "raw" >>> p_until_str_parsed "%>"
+      >>| (fun expr -> BlockOfRawExpr expr)
+      ||| (p_until_str_parsed "%>" >>| fun expr -> BlockOfExpr expr) )
+  ||| (p_str "<%" >>> p_until_str_parsed "%>" >>| fun code -> BlockOfCode code)
+  ||| ( p_prev_char '\n' ||| p_pos 0
+      >>> p_until_failed p_whitespace
+      >>> p_char '%' >>> p_until_char_parsed '\n'
+      >>| fun code -> LineOfCode code )
 
 let template_of_string s =
-  let code_fragment =
-    p_str "<%="
-    >>> ( p_str "raw"
-        >>> p_str_until (p_str "%>")
-        >>= (fun (expr, _) -> return (BlockOfRawExpr expr))
-        ||| ( p_str_until (p_str "%>") >>= fun (expr, _) ->
-              return (BlockOfExpr expr) ) )
-    ||| ( p_str "<%" >>> p_str_until (p_str "%>") >>= fun (code, _) ->
-          return (BlockOfCode code) )
-    ||| ( check_prev (p_char '\n')
-        >>> return ()
-        ||| (p_pos 0 >>> return ())
-        >>> p_many p_whitespace >>> p_char '%'
-        >>> p_str_until (p_str "\n")
-        >>= fun (code, _) -> return (LineOfCode code) )
+  let cons_text text acc = if text = "" then acc else Text text :: acc in
+  let rec loop ((s, pos) as st) acc =
+    match p_until_parsed code_fragment st with
+    | Parsed { v = text, code; st } -> loop st (code :: cons_text text acc)
+    | Failed -> cons_text (String.slice ~first:pos s) acc |> List.rev
   in
-  let rec loop acc =
-    let text_then_code =
-      p_str_until code_fragment >>= fun (text, code) ->
-      (* FIXME *)
-      if text = "" then loop (code :: acc) else loop (code :: Text text :: acc)
-    in
-    (* no more code - return the rest *)
-    let rest (s, pos) =
-      let text = String.slice ~first:pos s in
-      let rev_r = if text = "" then acc else Text text :: acc in
-      return (List.rev rev_r) (s, String.length s)
-    in
-    text_then_code ||| rest
-  in
-  match loop [] (s, 0) with Failed -> assert false | Parsed { v = r; _ } -> r
-
-let print_template printer esc_printer chan =
-  List.iter (function
-    | LineOfCode s -> fprintf chan "%s\n" s
-    | BlockOfCode s -> fprintf chan "%s" s
-    | BlockOfExpr s -> fprintf chan "%s (%s);" esc_printer s
-    | BlockOfRawExpr s -> fprintf chan "%s (%s);" printer s
-    | Text s ->
-        let l = String.nsplit s "\n" in
-        let rec loop = function
-          | [ "" ] -> ()
-          | [ h ] -> fprintf chan "%s %S;" printer h
-          | h :: t ->
-              fprintf chan "%s %S;\n" printer (h ^ "\n");
-              loop t
-          | [] -> ()
-        in
-        loop l)
-
-open Arg
+  loop (s, 0) []
 
 let () =
   let source = ref "" in
   let dest = ref "" in
   let printer = ref "" in
+  (* TODO: no esc_printer *)
   let esc_printer = ref "" in
   let header = ref "" in
   let footer = ref "" in
@@ -100,7 +130,7 @@ let () =
   let l =
     [
       ( "-o",
-        Set_string dest,
+        Arg.Set_string dest,
         "FILE\tdestination file to output an OCaml code; default is \
          sourcename.ml" );
       ( "-p",
@@ -139,7 +169,7 @@ let () =
     output_string chan !header;
     output_char chan '\n' );
   if !add_directive then fprintf chan "# 1 %S\n" (Filename.basename !source);
-  print_template !printer !esc_printer chan t;
+  print !printer !esc_printer chan t;
   if !footer <> "" then (
     if !add_directive then (
       output_char chan '\n';
